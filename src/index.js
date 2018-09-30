a83828b9e11b75c3de3f15eba5f4a443dcc0b871
Tanya98 <31532666+Tanya98@users.noreply.github.com>
2018-09-30 19:53:05 +0600

    first-commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length <= 1) {
+    return false;
+  }
+  let br;
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    br = str[i];
+
+    if (bracketsConfig.indexOf(br)) {
+      stack.push(br);
+    }
+
+    if (stack.length === 0 || !bracketsConfig.indexOf(br)) {
+      return false;
+    }
+
+    else {
+      return true;
+    }
+
+  }
 }
